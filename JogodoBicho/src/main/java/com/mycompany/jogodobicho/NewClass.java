904ea0bb4c3acea2938b5e1472eb8232a497366d
Jackson Santos Nascimento <jacksonnascimento84@hotmail.com>
2023-01-16 08:36:15 -0300

    a

---
+++ JogodoBicho/src/main/java/com/mycompany/jogodobicho/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jogodobicho;
+
+/**
+ *
+ * @author Jackson
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        NewJFrame n = new NewJFrame();
+        n.setVisible(true);
+    }
+}
